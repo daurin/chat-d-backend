3957b0799d36639e6d56c64452a5ccb47453dd31
marvinh5 <marvinnnz@gmail.com>
2021-06-05 10:17:00 -0400

    Added pub sub for multi process and config service

---
+++ src/server/config_service.ts
@@ -0,0 +1,25 @@
+export class ConfigService {
+    values: {[key:string]: any};
+
+    constructor() {
+        this.values = {};
+        this.init();
+    }
+
+    async init() {
+        //load values from environment or a file or a http service
+        this.values = {
+        }
+    }
+
+    get<T>(key:string, defaultValue:T): T {
+        
+        if(Object.keys(this.values).indexOf(key)!=-1) {
+            return this.values[key];
+        }
+
+        if(!defaultValue) throw new Error(`${key} variable not configured correctly`);
+
+        return defaultValue;
+    }
+}
